5eeb28590d0815b84e0c9def2dced23919be9de9
ticturd <148925085+ticturd@users.noreply.github.com>
2025-11-17 14:30:36 +1100

    started views.py, added to index.html

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, redirect, url_for, flash
 from models import db, Movie # Also import your database model here
+import datetime
 
 # Define your routes inside the 'init_routes' function
 # Feel free to rename the routes and functions as you see fit
@@ -9,16 +10,32 @@ from models import db, Movie # Also import your database model here
 
 def init_routes(app):
 
-    @app.route('/', methods=['GET'])
+    @app.route('/', methods=['GET', 'POST'])
     def get_items():
         # This route should retrieve all items from the database and display them on the page.
-        return render_template('index.html', message='Displaying all items')
+        movies = Movie.query.all()
+        return render_template('index.html', message='Displaying all items', movies=movies)
 
 
 
     @app.route('/add', methods=['POST'])
-    def create_item():
+    def create_movie():
         # This route should handle adding a new item to the database.
+        if request.method == 'POST':
+            new_movie = Movie(
+                title = request.form['title'],
+                director = request.form['director'],
+                genre = request.form['genre'],
+                release_date = datetime.date(request.form['release_date']),
+                cast = request.form['cast'],
+                image = request.form['image']
+            )
+            db.session.add(new_movie)
+            db.session.commit()
+            return redirect(url_for('index'))
+
+        
+
         return render_template('index.html', message='Item added successfully')
 
 
