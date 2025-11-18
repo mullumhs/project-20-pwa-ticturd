478f8175d55ae50f6a547790a860ed192626e2e0
ticturd <148925085+ticturd@users.noreply.github.com>
2025-11-18 14:11:04 +1100

    fixed add function, changed some variables inside the movie class, tried to create an update function and a delete function to no avail

--- models.py
+++ models.py
@@ -8,13 +8,12 @@ db = SQLAlchemy()
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    director = db.Column(db.Text)
+    director = db.Column(db.Text, nullable = False)
     genre = db.Column(db.Text, nullable=False)
-    release_date = db.Column(db.Date, nullable=False)
+    year = db.Column(db.Integer, nullable = False)
     rating = db.Column(db.Float, nullable=False)
-    cast = db.Column(db.List)
     image = db.Column(db.String, nullable = True) #Store in static/uploads, generate a unique file name for it, store reference in database
-
+    description = db.Column(db.String, nullable = False)
 
 
 
--- views.py
+++ views.py
@@ -26,13 +26,13 @@ def init_routes(app):
                 title = request.form['title'],
                 director = request.form['director'],
                 genre = request.form['genre'],
-                release_date = datetime.date(request.form['release_date']),
-                cast = request.form['cast'],
-                image = request.form['image']
+                year = int(request.form['year']),
+                rating = float(request.form['rating']),
+                image = request.form['image'],
+                description = request.form['description'],
             )
             db.session.add(new_movie)
             db.session.commit()
-            return redirect(url_for('index'))
 
         
 
@@ -43,6 +43,14 @@ def init_routes(app):
     @app.route('/update', methods=['POST'])
     def update_item():
         # This route should handle updating an existing item identified by the given ID.
+        if request.form == 'POST':
+            movie_id = request.form['id']
+            movie = Movie.query.get_or_404(movie_id)
+
+            movie.title = request.form['name']
+
+            db.session.commit()
+
         return render_template('index.html', message=f'Item updated successfully')
 
 
@@ -50,4 +58,11 @@ def init_routes(app):
     @app.route('/delete', methods=['POST'])
     def delete_item():
         # This route should handle deleting an existing item identified by the given ID.
+        if request.form == 'POST':
+            movie_id = request.form['id']
+            movie = Movie.query.get_or_404(movie_id)
+
+            db.session.delete(movie)
+            db.session.commit()
+
         return render_template('index.html', message=f'Item deleted successfully')
