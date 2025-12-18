a725d69ce31ca6428b13df54f04e1d8f0be65bd0
ticturd <148925085+ticturd@users.noreply.github.com>
2025-12-19 10:36:55 +1100

    cleaned up some code, stopped return url as a html, added some code commenting. almost finished with project.

--- views.py
+++ views.py
@@ -29,7 +29,7 @@ def init_routes(app):
             existing_user = User.query.filter_by(username=username).first()
             if existing_user != None:
                 flash("Username already exists!", 'danger')
-                return redirect(url_for('register.html'))
+                return redirect(url_for('register'))
             
             #Creates account, directs user to log in.
             else:
@@ -47,7 +47,7 @@ def init_routes(app):
         return render_template('register.html')
         
         
-    
+    #Login
     @app.route('/login', methods=['GET', 'POST'])
     def login():
         if request.method == 'POST':
@@ -77,7 +77,7 @@ def init_routes(app):
     @app.route('/dashboard', methods=['GET', 'POST'])
     def dashboard():
         #Gets logged in user id
-        user_id = session.get('user_id')
+        user_id = session.get('user_id', None)
         if not user_id:
             return render_template('login.html', message="Please log in or register.")
         
@@ -87,14 +87,17 @@ def init_routes(app):
         movies = Movie.query.filter(Movie.is_deleted == False, Movie.user_id == user_id).all()
         return render_template('dashboard.html', movies=movies, user=user, filter_choice='title')
     
+    #Search
     @app.route('/search', methods=['GET'])
     def search():
         user_id = session.get('user_id')
         user = User.query.get(user_id)
 
+        #Gets filter choice entered in the website.
         filter_choice = request.args.get('filter', 'title')
         search_query = request.args.get('search', '')
 
+        #Searches using %. .ilike() acts as SQLite's 'LIKE' for string and text columns.
         if search_query.strip():
             if filter_choice == 'director':
                 search_results = Movie.query.filter(Movie.user_id == user_id, Movie.director.ilike(f"%{search_query}%")).all()
@@ -103,19 +106,19 @@ def init_routes(app):
                 search_results = Movie.query.filter(Movie.user_id == user_id, Movie.genre.ilike(f"%{search_query}%")).all()
 
             elif filter_choice == 'year':
-                search_results = Movie.query.filter(Movie.user_id == user_id, Movie.year.ilike(f"%{search_query}")).all()
+                search_results = Movie.query.filter(Movie.user_id == user_id, Movie.year ==  int(f"%{search_query}")).all()
 
             elif filter_choice == 'rating':
-                search_results = Movie.query.filter(Movie.user_id == user_id, Movie.rating.ilike(f"%{search_query}%")).all()
+                search_results = Movie.query.filter(Movie.user_id == user_id, Movie.rating ==  float(f"%{search_query}%")).all()
 
             else:
                 search_results = Movie.query.filter(Movie.user_id == user_id, Movie.title.ilike(f"%{search_query}%")).all()
 
             return render_template('dashboard.html', movies = search_results, user=user, filter_choice = filter_choice)
         
-        else:
-            movies = Movie.query.filter_by(user_id=user_id).all()
-            return render_template('dashboard.html', movies=movies, user=user, filter_choice=filter_choice)
+        
+        movies = Movie.query.filter_by(user_id=user_id).all()
+        return render_template('dashboard.html', movies=movies, user=user, filter_choice=filter_choice)
 
 
 
@@ -138,14 +141,16 @@ def init_routes(app):
             image_file = request.files.get("image_file")
             image_url = request.form.get("image_url")
 
-
+            #Handles storage of the image file in the database if user decides to drag and drop or upload.
             if image_file and image_file.filename != "":
                 filename = secure_filename(image_file.filename)
                 filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
                 image_file.save(filepath)
 
+                
                 image_path = f'static/uploads/{filename}'
 
+            #Handles image storage if image is a URL.
             elif image_url:
                 image_path = image_url
 
@@ -198,8 +203,8 @@ def init_routes(app):
         movie.title = request.form["title"]
         movie.director = request.form["director"]
         movie.genre = request.form["genre"]
-        movie.year = request.form["year"]
-        movie.rating = request.form["rating"]
+        movie.year = int(request.form["year"])
+        movie.rating = float(request.form["rating"])
         movie.description = request.form["description"]
         movie.image = image_path
         
@@ -210,27 +215,33 @@ def init_routes(app):
         return redirect(url_for('dashboard'))
 
 
-
+    #'Deletes' specified movie.
     @app.route('/delete', methods=['POST'])
     def delete():
-        #'Deletes' specified movie.
-
         movie_id = request.form['id']
         movie = Movie.query.get_or_404(movie_id)
-
+        
+        #Checks whether the movie being deleted is being deleted by the user, to prevent hackers from guessing an ID and getting their movie removed.
+        if movie.user_id != session.get('user_id'):
+            flash(f'Unauthorized access. Unable to delete {movie.title}.', 'danger')
+            return redirect(url_for('dashboard'))
+        
+        #Movie still exists in database, just not visible to users.
         movie.is_deleted = True
 
         db.session.commit()
 
+        #Flashes a message with an 'undo' link, allowing users to recover their deleted movie. However, there is no way to restore after a refresh.
         flash(f'Movie "{movie.title}" deleted. <a href="{url_for("undo_delete", id=movie_id)}">Undo</a>', 'warning')
 
         return redirect(url_for('dashboard'))
     
-
+    #Grabs movie id from the /delete route.
     @app.route('/undo_delete/<int:id>', methods=['GET'])
     def undo_delete(id):
         movie = Movie.query.get_or_404(id)
 
+        #Movie becomes visible to users again.
         movie.is_deleted = False
 
         flash(f'Movie "{movie.title}" restored!', 'success')
